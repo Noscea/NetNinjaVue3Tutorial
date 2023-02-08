2d907961a90343d73a3185e34fe5eeb06d6c7fd2
Noscea <corentindumontier@gmail.com>
2023-02-08 18:05:25 +0100

    feat:Ajout de l'attribut binding :sparkles:

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ const app = Vue.createApp({
   //Initialisation des variables du composant
   data() {
     return {
-      showBooks:true,
-      title: "The Final Empire",
-      author: "Brandon Sanderson",
-      age: 45,
-      x:0,
-      y:0
+      url:"http://www.thenetninja.co.uk",
+      url2:"https://www.youtube.com/watch?v=CYPZBK8zUik&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=3",
+      showBooks: true,
+      books: [
+        { title: "The Final Empire", author: "Brandon Sanderson", image: 'assets/1.jpg'},
+        { title: "The Way of Kings", author: "Brandon Sanderson", image: 'assets/2.jpg'},
+        { title: "Name of the wind", author: "Patrick Rothfuss" , image: 'assets/3.jpg'},
+      ],
+      age:45,
+      x: 0,
+      y: 0,
     };
   },
   //Déclarations des methodes du composant
@@ -20,20 +25,20 @@ const app = Vue.createApp({
     },
 
     toggleShowBooks() {
-      this.showBooks = !this.showBooks
+      this.showBooks = !this.showBooks;
     },
 
     handleEvent(event, data) {
       console.log(event, event.type);
-      if (data){
+      if (data) {
         console.log(data);
       }
     },
 
     handleMouseMove(event) {
-      this.x = event.offsetX
-      this.y = event.offsetY
-    }
+      this.x = event.offsetX;
+      this.y = event.offsetY;
+    },
   },
 });
 
