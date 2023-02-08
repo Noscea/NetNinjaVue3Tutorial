1ab5989179f9e7a3bef94681929f7f525ad51df9
Noscea <corentindumontier@gmail.com>
2023-02-08 18:16:26 +0100

    feat:Ajout des classes conditionnels avec un v-bind :sparkles:

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const app = Vue.createApp({
       url2:"https://www.youtube.com/watch?v=CYPZBK8zUik&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=3",
       showBooks: true,
       books: [
-        { title: "The Final Empire", author: "Brandon Sanderson", image: 'assets/1.jpg'},
-        { title: "The Way of Kings", author: "Brandon Sanderson", image: 'assets/2.jpg'},
-        { title: "Name of the wind", author: "Patrick Rothfuss" , image: 'assets/3.jpg'},
+        { title: "The Final Empire", author: "Brandon Sanderson", image: 'assets/1.jpg', isFav:true},
+        { title: "The Way of Kings", author: "Brandon Sanderson", image: 'assets/2.jpg', isFav:false},
+        { title: "Name of the wind", author: "Patrick Rothfuss" , image: 'assets/3.jpg', isFav:true},
       ],
       age:45,
       x: 0,
