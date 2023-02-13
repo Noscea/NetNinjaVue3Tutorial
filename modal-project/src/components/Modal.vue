3961ea9c405e4facb1f548bfd82b54f7c1a92f8b
Noscea <corentindumontier@gmail.com>
2023-02-13 12:10:08 +0100

    feat:Appréciation du style en Vue - Style scoped dans les composants et styles globals dans les fichier séparés :feat:

--- modal-project/src/components/Modal.vue
+++ modal-project/src/components/Modal.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="backdrop">
         <div class="modal">
+            <h1>Modal Content</h1>
             <p>Modal Content</p>
         </div>
     </div>
 </template>
 
-<style>
+<style scoped>
 .modal{
     width: 400px;
     padding: 20px;
@@ -21,5 +22,9 @@
     background: rgba(0,0,0,0.5);
     width: 100%;
     height: 100%;
+}
+
+h1{
+    color: green;
 }
 </style>
