4d1bdd47df6d317441e3c2c6e4bfcf29077e31d9
Noscea <corentindumontier@gmail.com>
2023-02-13 12:36:04 +0100

    feat: Ajout des premiers event personnalisés :sparkles:

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <Modal :header="header" :text="text" theme="sale" />
+  <h1>{{ title }}</h1>
+  <p>Welcome</p>
+  <div v-if="showModal">
+    
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal"/>
   </div>
+  <button @click="toggleModal">Show Modal</button>
 </template>
 
 <script>
@@ -17,7 +20,8 @@ export default {
     return {
       title: "My First Vue App",
       header: "Sign up for a giveaway",
-      text:"you'll win for sure"
+      text:"you'll win for sure",
+      showModal:false
     };
   },
   methods: {
@@ -27,6 +31,9 @@ export default {
       this.$refs.name.classList.add("active");
       this.$refs.name.focus();
     },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    }
   },
 };
 </script>
